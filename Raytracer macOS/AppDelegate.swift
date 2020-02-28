fa128028ebce635a4a0ca06fbdf573333f37fafd
Brad Feehan <git@bradfeehan.com>
2020-02-28 21:45:20 +1100

    Initial Commit

---
+++ Raytracer macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Raytracer macOS
+//
+//  Created by Brad Feehan on 28/2/20.
+//  Copyright © 2020 Brad Feehan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
